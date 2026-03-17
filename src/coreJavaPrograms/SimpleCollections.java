e8eaf824538c024bfdf9d27bcc0e62ae44d8a801
NagKothapalli <nag022@gmail.com>
2026-03-17 22:30:33 +0530

    March17th code

---
+++ src/coreJavaPrograms/SimpleCollections.java
@@ -0,0 +1,76 @@
+package coreJavaPrograms;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class SimpleCollections {
+	
+	//Array - group of elements with similar data types
+	
+	//group of students - names / number / grades 
+	
+	int rollNum = 22;   // single value
+	
+	int[]  rollNUmbers = {12,14,13,7,6,9,80,23}; // static array or fixed array  // group of values 
+	
+	//Declare , assign , length , read values , write values in to the array 
+	@Test
+	public void fixedArray() {
+		int[]  rollNumbers = {12,14,13,7,6,9,80,23};
+		int count = rollNumbers.length;
+		System.out.println("Array Length :" + count);
+		// i=0 1 2 3 4 5 6 7 ... 8th index
+		for(int i=0;i<=count;i++) {
+			System.out.println(rollNumbers[i]);
+		}
+		System.out.println("--------------------------------");
+		for(int num:rollNumbers) {
+			System.out.println(num);
+		}
+	}
+	
+	//Dynamic Array - 40 students but their total marks is not known 
+	@Test
+	public void dynamicArray() {
+		int[] students = new int[40];
+		for(int i=0;i<students.length;i++) {
+			students[i] = i+22; // 0+22 , 1+22 , 2+22 ..i..i...i..i..i.
+		}
+		for(int i=0;i<students.length;i++) {
+			System.out.println(students[i]);
+		}
+	}
+	
+	
+	//Need the student numbers who got distinction  - here we use arraylist which is a dynamic collection
+	//Generic types are wrapper classes on top of primitive data types in java 
+	 // int -> Integer   , boolean -> Boolean , String 
+	//List  - interface , implemented by different classes like ArrayList , LinkedList  , list will allow duplicate values 
+	//Set  - will not allow duplicate values 
+	@Test
+	public void myArrayList() {
+		ArrayList<Integer>  mylist = new ArrayList<Integer>();
+		for(int i=0;i<20;i++) {
+			mylist.add(22+i);  // at index 0 -> 22 1  2 3 4 ....10
+		}
+		
+		for(int i=0;i<20;i++) {
+			System.out.println(mylist.get(i));
+		}
+		
+		mylist.remove(2);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/seleniumPractice/GmailAutomation.java
+++ src/seleniumPractice/GmailAutomation.java
@@ -1,7 +1,11 @@
 package seleniumPractice;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,11 +14,12 @@ import junit.framework.Assert;
 
 public class GmailAutomation {
 	
-	@Test
+	ChromeDriver  driver = new ChromeDriver();
+	@Before  //this will be executed before every test method
 	public void launchApplication() throws InterruptedException {
 		System.out.println("Launch Gmail Application");
 		//open google chrome browser
-		ChromeDriver  driver = new ChromeDriver(); // it will open an empty google chrome browser
+		 // it will open an empty google chrome browser
 		//FirefoxDriver  driver = new FirefoxDriver(); 
 		//EdgeDriver driver = new EdgeDriver();
 		driver.get("https://gmail.com");
@@ -31,20 +36,134 @@ public class GmailAutomation {
 		 * if(title.equals("Gmaill")) { System.out.println("Title validation Passed"); }
 		 * else { System.out.println("Title Validation Failed"); }
 		 */
-		driver.findElement(By.id("identifierId")).click();
-		driver.findElement(By.id("identifierId")).sendKeys("afhskjfhdsjlfkdsj");
+		//driver.findElement(By.id("identifierId")).click();
+		//driver.findElement(By.id("identifierId")).sendKeys("afhskjfhdsjlfkdsj");
+		//Thread.sleep(2000);
+		//driver.findElement(By.id("identifierId")).clear();
+		//Thread.sleep(2000);
+		//driver.findElement(By.id("identifierId")).sendKeys("nag022@gmail.com");
+		
+	}
+	//selenium selectors / locators : id , name , class , cssSelector , linkText , partialLinkText , xpath
+	//Different webelements : textbox , button , radio btn , chk box , list ,frame 
+	@Test
+	public void loginToApplication() throws InterruptedException {
+		//launchApplication();
+		System.out.println("Login to Application");
+		//enter username
+		WebElement email = driver.findElement(By.name("identifier"));
+		email.click();
+		email.clear();
+		email.sendKeys("dsfsdfsfsfsdffsf");
 		Thread.sleep(2000);
-		driver.findElement(By.id("identifierId")).clear();
+		email.clear();
+		email.sendKeys("nag022@gmail.com");
 		Thread.sleep(2000);
-		driver.findElement(By.id("identifierId")).sendKeys("nag022@gmail.com");
+		driver.findElements(By.className("VfPpkd-vQzf8d")).get(1).click();
+	}
+	@Test
+	public void learnMore() {
+		//  Learn more about using Guest mode
+		//driver.findElement(By.linkText("Learn more about using Guest mode")).click();
+		driver.findElement(By.partialLinkText("Learn more about using")).click();
+		
+	}
+	//xpath -> Absolute xpath :   //*[@id="yDmH0d"]/c-wiz/main/div[2]/div/div/div[1]/span/section/div/div/div[3]/button
+	
+	//Relative xpath :  //button[text()='Forgot email?']
+	//tagName[text()='']
+	//tagName[@id=""]
+	//tagName[@name=""]
+	//tageName[@id='' && text()='fdfsdfhj']
+	//tagName[contains(text(),'dfsfsfs')]
+	//tagName[contains(id,'dfsfsfs')]
+	//tagName[contains(name,'dfsfsfs')]
+	@Test
+	public void forgotEmail() {
+		//driver.findElement(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/main/div[2]/div/div/div[1]/span/section/div/div/div[3]/button")).click();
+		driver.findElement(By.xpath("//button[text()='Forgot email?']")).click();
+	}
+	
+	//  /html/body/div[2]/div[1]/div[1]/div[2]/c-wiz/main/div[2]/div/div/div[1]/span/section/div/div/div[1]/div[1]/div[1]/div/div[1]/input
+	@Test
+	public void absoluteXpath() {
+		driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[1]/div[2]/c-wiz/main/div[2]/div/div/div[1]/span/section/div/div/div[1]/div[1]/div[1]/div/div[1]/input")).sendKeys("nag022");
+	}
+	@Test
+	public void enterEmail() {
+		String myxpath = "/html/body/div[2]/div[1]/div[1]/div[2]/c-wiz/main/div[2]/div/div/div[1]/span/section/div/div/div[1]/div[1]/div[1]/div/div[1]/input";
+		driver.findElement(By.xpath(myxpath)).sendKeys("nag022@gmail.com");
+	}
+	@Test
+	public void enterEmailWithRelativeXpath() {
+		//xpath1 -   //input[@name='identifier']		
+		//driver.findElement(By.xpath("//input[@name='identifier']")).sendKeys("nag022@gmail.com");
+		String xpath2 = "//input[@id='identifierId']";
+		//driver.findElement(By.xpath(xpath2)).sendKeys("nag022@gmail.com");
+		String xpath3 = "//input[@type='email']";
+		//driver.findElement(By.xpath(xpath3)).sendKeys("nag022@gmail.com");
+		String xpath4 = "//input[@aria-label='Email or phone']";
+		//driver.findElement(By.xpath(xpath4)).sendKeys("nag022@gmail.com");
+		//String xpath5 = "//div[text()='Email or phone']";
+		//driver.findElement(By.xpath(xpath5)).sendKeys("nag022@gmail.com");
+	}
+	//org.openqa.selenium.ElementNotInteractableException: element not interactable
+	
+	@Test
+	public void DemoNoSuchElementException() {
+		String xpath4 = "//input[@aria-label='Email or phoneNumber']";
+		driver.findElement(By.xpath(xpath4)).sendKeys("nag022@gmail.com");
+		//org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {"method":"xpath","selector":"//input[@aria-label='Email or phoneNumber']"}
+	}
+	
+	//WebElement is an interface in webdriver , it contains methods like click , clear , sendkeys ,
+	    // isDisplayed , isEnabled , isSelected , findElement , findElements 
+	
+	// findElement , findElements  are the methods available in two interfaces - WebDriver , WebElement
+	@Test
+	public void containsOptionInRelativeXpath() {
+		String myxpath = "//input[contains(@aria-label,'Email')]";
+		//driver.findElement(By.xpath(myxpath)).sendKeys("nag022@gmail.com");
+		WebElement email = driver.findElement(By.xpath("//input[contains(@aria-label,'Email')]"));
+		String mytext = email.getText();
+		System.out.println("My text on the object :" + mytext);
+		//email.isDisplayed();
+		if(email.isDisplayed()  && email.isEnabled()) {
+			email.click();
+			email.sendKeys("nag022@gmail.com");
+		}
 		
 	}
 	
-	public void loginToApplication() {
-		System.out.println("Login to Application");
-		//enter username
+	@Test
+	public void readAttributes() {		
+	WebElement element = driver.findElement(By.xpath("//button[text()='Forgot email?']"));
+	System.out.println(element.getText());
+	System.out.println(element.getTagName());
+	System.out.println(element.getAttribute("jsname"));
+	System.out.println(element.getAttribute("type"));
 	}
 	
+	//how many links / hyperlinks are there in the given web page 
+	// all links will start with anchor tag - //a
+	@Test
+	public void getAllWebLinksInThePage() {
+		List<WebElement> links = driver.findElements(By.xpath("//a"));
+		System.out.println("Number of hyperlinks :" + links.size());
+		//normal for loop
+		for(int i=0;i<links.size();i++) {
+			System.out.println(i);
+			System.out.println(links.get(i).getText());
+			if(links.get(i).getText().equals("Privacy")) {
+				links.get(i).click();
+				break;
+			}
+		}
+		//System.out.println("----------------------------");
+		// foreach loop
+		/*
+		 * for(WebElement ele:links) { System.out.println(ele.getText()); }
+		 */	}
 	
 	
 	
